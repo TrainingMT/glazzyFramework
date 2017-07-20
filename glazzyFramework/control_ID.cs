2edfcfdcfe57d2bbadc5443ba94ae46175693da2
swapnil tripathi <swapnil.a.tripathi@gmail.com>
2017-07-20 16:17:12 -0700

    Commiting the base files

---
+++ glazzyFramework/control_ID.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace glazzyFramework
+{
+	class control_ID
+	{
+	}
+}
