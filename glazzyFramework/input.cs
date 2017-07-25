bceb59765d6165d07d5bd0140b1437cec0670b65
Saydinyan <stan.aydinyan@gmail.com>
2017-07-24 18:08:43 -0700

    Update input.cs

--- glazzyFramework/input.cs
+++ glazzyFramework/input.cs
@@ -8,16 +8,28 @@ namespace glazzyFramework
 {
 	class input
 	{
-		        //Update your profile Values
+	
+		
+	//Update Profile Input
+	//Enter Username
         public static string username = "Tester@gmail.com";
+	//Enter Firstname
         public static string firstname = "John";
+	//Enter LastName
         public static string lastname = "Doe";
+	//Select DOB
         public static string dob = "06/26/1986";
+	//Enter Address
         public static string address = "TesterDr";
+	//Select State
         public static string state = "CA";
+	//Select City
         public static string city = "San Diego";
+	//Enter Zip
         public static string zip = "92117";
+	//Enter Phone Number
         public static string phone = "2211234567";
+	//Click Save Button
         public static string save = "buttonclick";
 	
 	
